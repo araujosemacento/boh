7d3515b21db8e7354ce7fff5dd6e88e220a1cfca
G. Melo <araujosemacento@alu.ufc.br>
2026-06-16 02:03:18 -0300

    Add Catppuccin themes, daisyUI and Settings UI
    Introduce Catppuccin daisyUI themes and a theme picker UI.

---
+++ src/lib/components/Settings.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { Palette, IceCreamBowl, Soup, CupSoda, Beaker, Coffee } from '@lucide/svelte';
+</script>
+
+<div class="dropdown">
+	<div tabindex="0" role="button" class="btn btn-secondary m-1 p-2"><Palette /></div>
+	<ul tabindex="-1" class="dropdown-content bg-base-300 rounded-box w-40 z-1 p-2 shadow-2xl">
+		<li>
+			<IceCreamBowl />
+			<input
+				type="radio"
+				name="theme-dropdown"
+				class="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+				aria-label="Padrão"
+				value="default"
+				checked
+			/>
+		</li>
+		<li>
+			<Soup />
+			<input
+				type="radio"
+				name="theme-dropdown"
+				class="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+				aria-label="Latte"
+				value="latte"
+			/>
+		</li>
+		<li>
+			<CupSoda />
+			<input
+				type="radio"
+				name="theme-dropdown"
+				class="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+				aria-label="Frappé"
+				value="frappe"
+			/>
+		</li>
+		<li>
+			<Beaker />
+			<input
+				type="radio"
+				name="theme-dropdown"
+				class="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+				aria-label="Macchiato"
+				value="macchiato"
+			/>
+		</li>
+		<li>
+			<Coffee />
+			<input
+				type="radio"
+				name="theme-dropdown"
+				class="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+				aria-label="Mocha"
+				value="mocha"
+			/>
+		</li>
+	</ul>
+</div>
